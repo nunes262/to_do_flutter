3526583d8bc5198433889befd3ea456a0bacfca0
pedronunes-devrise <pedronunes.devrise@gmail.com>
2023-09-25 15:16:45 -0300

    colors

--- lib/constants/Colors.dart
+++ lib/constants/Colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const Color tdRed = Color.fromARGB(255, 255, 15, 15);
-const Color tdBlue = Color.fromARGB(255, 108, 5, 204);
+const Color tdBlue = Color.fromARGB(255, 0, 110, 255);
 
 const Color tdBlack = Color.fromARGB(255, 0, 0, 0);
 const Color tdGray = Color.fromARGB(255, 133, 133, 133);
